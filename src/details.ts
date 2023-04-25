8fa0cb634ca5c971de51445dc751e02d5a2b05c3
Gergely Kovacs <gergely.kovacs@goodgrind.tech>
2023-04-25 16:43:17 +0200

    added some data and interfaces

---
+++ src/details.ts
@@ -0,0 +1,46 @@
+export type Position =
+  | "Software Engineer Trainee"
+  | "Junior Software Engineer"
+  | "Software Engineer"
+  | "Senior Software Engineer";
+
+export interface EmploymentPeroid {
+  id: number;
+  name: string;
+  positions: Position[];
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Project {
+  employmentPeriodId: number;
+  name: string;
+  goal: string;
+  technologies: string[];
+  tasks: string[];
+  duration_months: number;
+}
+
+export const employmentPeriods: EmploymentPeroid[] = [
+  {
+    id: 1,
+    name: "AdNovum Hungary Kft",
+    positions: ["Software Engineer Trainee", "Junior Software Engineer"],
+    startDate: new Date("2016-04-01"),
+    endDate: new Date("2017-11-01"),
+  },
+];
+
+export const details: Project[] = [
+  {
+    employmentPeriodId: 1,
+    name: "Trainee project",
+    goal: "Investigating RxJava integration into the company toolbox",
+    technologies: ["SVN", "Maven", "Java", "JavaScript", "RxJava"],
+    tasks: [
+      "Creating some web app that utilizes RxJava",
+      "Holding a presentation about the app and my findings to my colleagues",
+    ],
+    duration_months: 2,
+  },
+];
