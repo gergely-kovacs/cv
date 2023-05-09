e00ade7682e54f5952a6803076b43c6ab4b9ddc1
Gergely Kovacs <gergely-kovacs@hotmail.com>
2023-05-09 22:54:40 +0200

    implemented tech exp summing logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import DarkModeToggler from "./DarkModeToggler";
 import { details } from "./details";
 
 function App() {
-  const jobSummary = details.map((jobDetails) => {
-    // TODO: actually summarize how much experience you have with each tech
-    return <div key={jobDetails.id}>{jobDetails.name}</div>;
+  const jobSummary = details.reduce((summary, jobDetails) => {
+    jobDetails.technologies.forEach((tech) => {
+      if (summary[tech]) {
+        summary[tech] += jobDetails.duration_months;
+        return;
+      }
+      summary[tech] = jobDetails.duration_months;
+    });
+    return summary;
+  }, {} as Record<string, number>);
+  const experienceSummary = Object.entries(jobSummary).map(([key, value]) => {
+    return (
+      <div key={key}>
+        <span className="inline-block w-36 font-semibold">{key}:</span>
+        {value} months
+      </div>
+    );
   });
 
   return (
@@ -17,30 +31,30 @@ function App() {
       </div>
       <div className="py-5 text-stone-900 dark:text-sky-100">
         <p>
-          <span className="inline-block w-36 font-semibold">First name:</span> Gergely
+          <span className="inline-block w-36 font-semibold">First name:</span>Gergely
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Last name:</span> Kovacs
+          <span className="inline-block w-36 font-semibold">Last name:</span>Kovacs
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Date of birth:</span> 1992. 04. 11.
+          <span className="inline-block w-36 font-semibold">Date of birth:</span>1992. 04. 11.
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Nationality:</span> hungarian
+          <span className="inline-block w-36 font-semibold">Nationality:</span>hungarian
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Highest degree:</span> Computer Science Engineering (BSc)
+          <span className="inline-block w-36 font-semibold">Highest degree:</span>Computer Science Engineering (BSc)
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Email:</span> gergely-kovacs@hotmail.com
+          <span className="inline-block w-36 font-semibold">Email:</span>gergely-kovacs@hotmail.com
         </p>
         <p>
-          <span className="inline-block w-36 font-semibold">Phone:</span> +36 70 389 7606
+          <span className="inline-block w-36 font-semibold">Phone:</span>+36 70 389 7606
         </p>
         {/* TODO: add website link */}
       </div>
       <CategoryHeader text="Experience"></CategoryHeader>
-      <div className="py-5 text-stone-900 dark:text-sky-100">{jobSummary}</div>
+      <div className="py-5 text-stone-900 dark:text-sky-100">{experienceSummary}</div>
     </div>
   );
 }
