e183a7d860ad136a8ef1363516a06002ab8128b9
Gergely Kovacs <gergely.kovacs@goodgrind.tech>
2023-07-02 18:16:48 +0200

    inlined the icon svgs, created signal for theme

--- src/DarkModeToggler.tsx
+++ src/DarkModeToggler.tsx
@@ -1,13 +1,12 @@
-import { createEffect } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 
 export type ThemeOption = 'dark' | 'light';
 
 function DarkModeToggler() {
     const STORAGE_KEY = 'theme';
+    const DEFAULT_THEME = 'dark';
 
-    // FIXME: create signal for theme
-    // https://www.solidjs.com/tutorial/introduction_effects
-    let theme: ThemeOption = 'dark';
+    const [theme, setTheme] = createSignal(getColorPreference());
 
     function getColorPreference(): ThemeOption {
         if (!localStorage.getItem(STORAGE_KEY)) {
@@ -20,7 +19,7 @@ function DarkModeToggler() {
         }
 
         console.error(`Invalid theme preference: ${themeFromStorage}`);
-        return 'dark';
+        return DEFAULT_THEME;
     }
 
     function setColorPreference(theme: ThemeOption) {
@@ -29,23 +28,30 @@ function DarkModeToggler() {
     }
 
     function toggleDarkMode() {
-        theme = theme === 'dark' ? 'light' : 'dark';
-        setColorPreference(theme);
+        setTheme(theme() === 'dark' ? 'light' : 'dark');
+        setColorPreference(theme());
     }
 
     createEffect(() => {
-        theme = getColorPreference();
-        setColorPreference(theme);
+        setColorPreference(theme());
     });
 
-    // FIXME: colour the icons
     return (
         <div class="mx-4 print:hidden">
             <button type="button" title="Toggle dark mode" aria-hidden="true" onClick={toggleDarkMode}>
-                {theme === 'dark' ? (
-                    <img class="h-6 w-6" src="/src/assets/icons/moon.svg" alt="dark mode" />
+                {theme() === 'dark' ? (
+                    <div class="h-6 w-6 text-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+                            <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
+                        </svg>
+                    </div>
                 ) : (
-                    <img class="h-6 w-6" src="/src/assets/icons/sun.svg" alt="light mode" />
+                    <div class="h-6 w-6 text-amber-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                        </svg>
+                    </div>
                 )}
             </button>
         </div>
