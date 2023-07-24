9b88203fb32b9a391e5d81cfdee4a75793d63a18
Gergely Kovacs <gergely-kovacs@hotmail.com>
2023-07-24 11:46:06 +0200

    improved responsiveness of personal details

--- src/PersonalDetails.tsx
+++ src/PersonalDetails.tsx
@@ -7,29 +7,43 @@ function PersonalDetails() {
                 <CategoryHeader text="Personal & Contact Details" />
             </div>
             <div class="mb-8 mt-4 text-gray-800 dark:text-neutral-400">
-                <p>
-                    <span class="inline-block w-64 font-semibold">First name:</span>Gergely
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Last name:</span>Kovacs
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Date of birth:</span>1992. 04. 11.
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Nationality:</span>hungarian
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Highest degree:</span>Computer Science Engineering
-                    (BSc)
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Email:</span>gergely-kovacs@hotmail.com
-                </p>
-                <p>
-                    <span class="inline-block w-64 font-semibold">Phone:</span>+36 70 389 7606
-                </p>
-                {/* TODO: add website link, add github account link */}
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">First name:</div>
+                    <div>Gergely</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Last name:</div>
+                    <div>Kovacs</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Date of birth:</div>
+                    <div>1992. 04. 11.</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Nationality:</div>
+                    <div>hungarian</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Highest degree:</div>
+                    <div>Computer Science Engineering (BSc)</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Email:</div>
+                    <div>gergely-kovacs@hotmail.com</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Phone:</div>
+                    <div>+36 70 389 7606</div>
+                </div>
+                <div class="flex flex-wrap justify-between">
+                    <div class="font-semibold">Github:</div>
+                    <div>
+                        <a target="_blank" href="https://github.com/gergely-kovacs">
+                            gergely-kovacs
+                        </a>
+                    </div>
+                </div>
+                {/* TODO: add website link*/}
             </div>
         </>
     );
