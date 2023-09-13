564dcc7e7693d07891ed9eeb8cd917ad37601dfd
Gergely Kovacs <gergely-kovacs@hotmail.com>
2023-09-13 11:21:38 +0200

    removed personal details from detailed view

--- src/DetailedView.tsx
+++ src/DetailedView.tsx
@@ -1,13 +1,11 @@
 import { Component } from 'solid-js';
 import EmploymentHistory from './EmploymentHistory';
 import NavHeader from './NavHeader';
-import PersonalDetails from './PersonalDetails';
 
 const DetailedView: Component = () => {
     return (
         <div class="container-sm mx-auto h-full max-w-screen-md p-5">
             <NavHeader />
-            <PersonalDetails />
             <EmploymentHistory />
         </div>
     );
