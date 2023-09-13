901e9fe9f51ea0306d31f187bed6f045df791e28
Gergely Kovacs <gergely-kovacs@hotmail.com>
2023-09-13 11:22:01 +0200

    added styling to links

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,11 +1,12 @@
 import { A } from '@solidjs/router';
 import DarkModeToggler from './DarkModeToggler';
+import headerStyles from './NavHeader.module.css';
 
 function NavHeader() {
     return (
         <div class="flex print:hidden">
             <A
-                class="inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm text-gray-700 hover:border-gray-400 dark:border-gray-500 dark:text-white dark:hover:border-gray-300 sm:text-base"
+                class={`${headerStyles.navLink} inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm hover:border-gray-400 dark:border-gray-500 dark:hover:border-gray-300 sm:text-base`}
                 activeClass="border border-b-0 rounded-t-md"
                 href="/cv/"
                 end={true}
@@ -14,7 +15,7 @@ function NavHeader() {
             </A>
 
             <A
-                class="inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm text-gray-700 hover:border-gray-400 dark:border-gray-500 dark:text-white dark:hover:border-gray-300 sm:text-base"
+                class={`${headerStyles.navLink} inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm hover:border-gray-400 dark:border-gray-500 dark:hover:border-gray-300 sm:text-base`}
                 activeClass="border border-b-0 rounded-t-md"
                 href="/cv/details"
                 end={true}
