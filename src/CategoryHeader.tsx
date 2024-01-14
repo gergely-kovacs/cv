e02de1fd5cb3f010edb02f8720fa37dad6289011
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-01-14 23:32:34 +0100

    category header can have nested html now

--- src/CategoryHeader.tsx
+++ src/CategoryHeader.tsx
@@ -1,12 +1,16 @@
+import { JSX } from 'solid-js';
+
 interface CategoryHeaderParams {
     text: string;
+    children?: JSX.Element;
 }
 
 function CategoryHeader(headerParams: CategoryHeaderParams) {
     return (
         <>
-            <h1 class="inline-block border-b-2 border-zinc-800 pb-1 text-2xl font-bold text-zinc-800 dark:border-neutral-300 dark:text-neutral-200">
-                {headerParams.text}
+            <h1 class="flex justify-between border-b-2 border-zinc-800 pb-1 text-2xl font-bold text-zinc-800 dark:border-neutral-300 dark:text-neutral-200">
+                <span>{headerParams.text}</span>
+                {headerParams.children && <span>{headerParams.children}</span>}
             </h1>
         </>
     );
