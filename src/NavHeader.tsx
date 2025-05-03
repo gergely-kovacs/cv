2426b8ac304a80d4b54487058fc836d01ecd9108
Gergely Kovacs <gergely-kovacs@hotmail.com>
2025-05-03 22:20:34 +0200

    fixed breaking tailwind changes

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,12 +1,11 @@
 import { A } from '@solidjs/router';
 import DarkModeToggler from './DarkModeToggler';
-import headerStyles from './NavHeader.module.css';
 
 function NavHeader() {
     return (
         <div class="flex print:hidden">
             <A
-                class={`${headerStyles.navLink} inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm hover:border-gray-400 dark:border-gray-500 dark:hover:border-gray-300 sm:text-base`}
+                class="inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm no-underline visited:text-sky-600 hover:border-gray-400 sm:text-base dark:border-gray-500 dark:visited:text-indigo-300 dark:hover:border-gray-300"
                 activeClass="border border-b-0 rounded-t-md"
                 href="/cv/"
                 end={true}
@@ -15,7 +14,7 @@ function NavHeader() {
             </A>
 
             <A
-                class={`${headerStyles.navLink} inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm hover:border-gray-400 dark:border-gray-500 dark:hover:border-gray-300 sm:text-base`}
+                class="inline-flex items-center border-b border-gray-300 px-4 py-2 text-sm no-underline visited:text-sky-600 hover:border-gray-400 sm:text-base dark:border-gray-500 dark:visited:text-indigo-300 dark:hover:border-gray-300"
                 activeClass="border border-b-0 rounded-t-md"
                 href="/cv/details"
                 end={true}
